94a2459310427caf2be2adf3f0ff16b22e57ecd6
Flávio Henrique <henriqueflavio55@gmail.com>
2023-01-23 18:38:45 -0300

    test with gitui

--- plugin/colors.lua
+++ plugin/colors.lua
@@ -2,8 +2,8 @@ function ColorMyPencils(color)
 	color = color or "nord"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFlot", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+	vim.api.nvim_set_hl(0, "NormalFlot", { bg = "NONE" })
 end
 
 ColorMyPencils()
