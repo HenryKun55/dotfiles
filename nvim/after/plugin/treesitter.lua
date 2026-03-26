4526bc45375b9b05fb4ecbc60d3ae13b8c894d7b
HenryKun55 <henriqueflavio55@gmail.com>
2026-03-26 16:13:51 -0300

    fix: treesitter

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,17 +1,20 @@
--- Install desired parsers (skips already installed)
-require("nvim-treesitter").install({
-  "javascript",
-  "typescript",
-  "rust",
-  "c",
-  "lua",
-  "http",
-  "json",
-  "markdown",
-  "python",
-  "vimdoc",
-  "luadoc",
-  "vim",
+-- Configure treesitter and install desired parsers
+require("nvim-treesitter.configs").setup({
+  ensure_installed = {
+    "javascript",
+    "typescript",
+    "rust",
+    "c",
+    "lua",
+    "http",
+    "json",
+    "markdown",
+    "python",
+    "vimdoc",
+    "luadoc",
+    "vim",
+  },
+  auto_install = true,
 })
 
 -- Treesitter highlight is enabled by default in Neovim 0.11+
