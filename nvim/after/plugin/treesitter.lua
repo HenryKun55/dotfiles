3396da6ccabf39327e441125b781736b978ffcea
HenryKun55 <henriqueflavio55@gmail.com>
2026-03-28 04:15:31 -0300

    updates

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 -- Configure treesitter and install desired parsers
-require("nvim-treesitter.configs").setup({
+require("nvim-treesitter.config").setup({
   ensure_installed = {
     "javascript",
     "typescript",
